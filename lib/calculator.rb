3047097c8a5f114dbaeeb8ead5c9bd43466fc48e
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-22 23:12:03 +0100

    Add more test using /bin/bash and __file__

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -17,28 +17,28 @@ class Calculator
 end
 
 
-# def leap_year?(year)
-#  year%400 == 0 or year%100 != 0 and year%4 == 0
-# end   
+def leap_year?(year)
+ year%400 == 0 or year%100 != 0 and year%4 == 0
+end   
 
-#   if $0 == __FILE__
-#     def assert_equal(expected, actual, method)
-#         if actual == expected
-#             puts "#{method} return #{actual} as expected"
-#         else
-#             puts "#{method} did not return #{expected} as expected but rather return #{actual}"
-#         end
-#     end
-#   end
+  if $0 == __FILE__
+    def assert_equal(expected, actual, method)
+        if actual == expected
+            puts "#{method} return #{actual} as expected"
+        else
+            puts "#{method} did not return #{expected} as expected but rather return #{actual}"
+        end
+    end
+  end
 
-#   data ={
-#       2001 => false,
-#       1900 => false,
-#       2000 => true,
-#       2004 => true
-#   }
+  data ={
+      2001 => false,
+      1900 => false,
+      2000 => true,
+      2004 => true
+  }
 
-#     data.each do |year, expected|
-#         actual = leap_year?(year)
-#         assert_equal(actual, expected, "leap_year?#{(year)}")
-#     end
+    data.each do |year, expected|
+        actual = leap_year?(year)
+        assert_equal(actual, expected, "leap_year?#{(year)}")
+    end
