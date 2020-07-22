63592b6f0eb205469a7ff2b882675e8d15a1a519
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-22 14:23:21 +0100

    wrote test for subtract,multiply,divide

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
     def add(a,b,c=0)  
         a + b + c
     end
+    
+   def subtract(a,b,c=0)
+        a-b-c
+   end
+
+    def multiply(a,b,c=1)
+        a * b * c 
+    end
+
+    def divide(a,b,c=1)
+        a.to_f / b.to_f / c.to_f
+    end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -13,4 +13,48 @@ describe Calculator do
       expect(calculator.add(2, 5, 7)).to eql(14)
     end
   end
+
+    #Subtraction
+
+    describe "#subtract" do
+        it "return subtraction of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.subtract(5,6)).to eql(-1)
+        end
+
+        it "return subtraction of three numbers" do
+            calculator = Calculator.new
+            expect(calculator.subtract(5,6,2)).to eql(-3)
+        end
+    end
+
+    #Multiplication
+
+    describe "#multiply" do
+        it "return multiplication of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.multiply(5,6)).to eql(30)
+        end
+
+        it "return multiplication of three numbers" do
+            calculator = Calculator.new
+            expect(calculator.multiply(5,6,2)).to eql(60)
+        end
+    end
+  
+    #Division
+
+    describe "#Division" do
+        it "return division of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.divide(5,6)).to eql(0.8333333333333334)
+        end
+
+        it "return Division of three numbers" do
+            calculator = Calculator.new
+            expect(calculator.divide(5,6,2)).to eql(0.4166666666666667)
+        end
+    end
 end
+
+
